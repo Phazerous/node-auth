ef2b7d897cec72aa116580e026c5e4092f23b764
Phazerous <nphazerous@gmail.com>
2023-01-30 01:38:19 +0600

    Added mongoose & mongodb online server
    Added .env & sign_up post

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 
 const authRoutes = require('./routes/authRoutes');
 
+const mongoose = require('mongoose');
+
+const dbURI = `mongodb+srv://${process.env.db_username}:${process.env.db_password}@cluster0.rotclkt.mongodb.net/`;
+
+mongoose.set('strictQuery', false);
+
+mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-
-app.listen(3000);
+app.use(express.json());
 
 app.use(authRoutes);
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,19 @@
+const User = require('../models/User');
+
 module.exports.signup_get = (req, res) => {
   res.render('signup');
 };
 
-module.exports.signup_post = (req, res) => {
-  res.send('New sign up');
+module.exports.signup_post = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.create({ email, password });
+    res.status(200).send(user);
+  } catch (err) {
+    res.status(400).send('Something went wrong');
+    console.log(err);
+  }
 };
 
 module.exports.login_get = (req, res) => {
