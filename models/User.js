f24e90abdbf6ee2b922f5afe2339bf278b01125f
Phazerous <nphazerous@gmail.com>
2023-01-31 01:22:15 +0600

    Sign up Validation and handler
    Validator + mongoose validation

--- models/User.js
+++ models/User.js
@@ -1,16 +1,18 @@
 const mongoose = require('mongoose');
+const { isEmail } = require('validator');
 
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
-    required: true,
+    required: [true, 'Please enter an email'],
     unique: true,
     lowercase: true,
+    validate: [isEmail, 'Please enter a valid email'],
   },
   password: {
     type: String,
-    required: true,
-    minLength: 6,
+    required: [true, 'Please enter a password'],
+    minLength: [6, `The password's length should be at least 8 characters.`],
   },
 });
 
