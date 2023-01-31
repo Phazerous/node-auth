b4681eb6dc952d58f6813171c1e10d7883c6749c
Phazerous <nphazerous@gmail.com>
2023-01-31 16:43:27 +0600

    JWT + Sign Up

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 
 const handleErrors = (err) => {
   const errors = { email: '', password: '' };
@@ -16,8 +17,12 @@ const handleErrors = (err) => {
   return errors;
 };
 
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.TOKEN_SECRET, { expiresIn: maxAge });
+};
+
 module.exports.signup_get = (req, res) => {
-  console.log('Fish');
   res.render('signup');
 };
 
@@ -26,7 +31,9 @@ module.exports.signup_post = async (req, res) => {
 
   try {
     const user = await User.create({ email, password });
-    res.status(200).send(user);
+    const token = createToken(user._id);
+    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.status(200).send({ user: user._id });
   } catch (err) {
     const errors = handleErrors(err);
     res.status(400).json(errors);
